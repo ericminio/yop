6f48fde103c8b1a4eb34e2f11df438f8387423c1
ericminio <eric.mignot@gmail.com>
2022-07-08 22:27:40 -0700

    dead code removed

--- utils/files.js
+++ utils/files.js
@@ -4,12 +4,6 @@ const path = require('path');
 const content = (file)=> fs.readFileSync(path.join(__dirname, '..', file)).toString()
 const code = (file, name)=> (new Function(`${content(file)}; return ${name};`))()
 
-const contents = (files)=> files.reduce((acc, file)=> acc += content(file), '');
-const codes = (files, name)=> (new Function(`${contents(files)}; return ${name};`))()
-
 module.exports = {
-    code: code,
-    codes: codes,
-    content: content,
-    contents: contents
+    code: code
 };
