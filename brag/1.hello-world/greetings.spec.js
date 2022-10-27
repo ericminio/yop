456681a477077a1e55c4bdb9cb5d811a74299338
ericminio <eric.mignot@gmail.com>
2022-10-27 09:22:51 -0700

    duplication removed

--- brag/1.hello-world/greetings.spec.js
+++ brag/1.hello-world/greetings.spec.js
@@ -4,7 +4,7 @@ import { page } from '../../lib/index.js';
 describe('Greetings message', () => {
 
     beforeEach(async () => {
-        await page.openFile('./brag/1.hello-world/index.html');
+        await page.open('./brag/1.hello-world/index.html');
     })
 
     it('is displayed', () => {
