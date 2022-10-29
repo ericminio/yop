001899a65781febff07811e4aa004061683a1f20
ericminio <eric.mignot@gmail.com>
2022-10-28 21:01:40 -0700

    file path fixed

--- brag/2.async/prime-factors.spec.js
+++ brag/2.async/prime-factors.spec.js
@@ -4,7 +4,7 @@ import { page, eventually } from '../../lib/index.js';
 describe('prime factors decomposition', () => {
 
     beforeEach(async () => {
-        await page.open('./brag/2.async-ui/index.html');
+        await page.open('./brag/2.async/index.html');
     });
 
     it('is available', async () => {
