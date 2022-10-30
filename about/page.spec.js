c935c397fb340b3a0f5209f72029c4274ac547bb
ericminio <eric.mignot@gmail.com>
2022-10-29 22:14:00 -0700

    more about page

--- about/page.spec.js
+++ about/page.spec.js
@@ -1,11 +1,35 @@
 import { expect } from 'chai';
-import { page } from '../lib/index.js';
+import { asset, page, Server } from '../lib/index.js';
 
 describe('page', () => {
 
-    it('can open html file', async () => {
-        await page.open('./about/page-index.html');
+    let file = './about/page-index.html';
 
-        expect(page.document.title).to.equal('page opening file');
+    describe('opening file', () => {
+
+        it('works', async () => {
+            await page.open(file);
+
+            expect(page.document.title).to.equal('well done page!');
+        });
+    });
+
+    describe('openining url', () => {
+
+        let server;
+        let port = 5001;
+        beforeEach(done => {
+            server = new Server(port, asset(file));
+            server.start(() => done());
+        });
+        afterEach(done => {
+            server.stop(done);
+        });
+
+        it('works', async () => {
+            await page.open(`http://localhost:${port}`);
+
+            expect(page.document.title).to.equal('well done page!');
+        });
     });
 });
