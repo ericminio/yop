947d43caaa774f087b7d0a2f4f7959ce9ad0fc26
ericminio <eric.mignot@gmail.com>
2022-10-29 23:44:57 -0700

    ux

--- brag/3.tests-pyramid/1.home-page.spec.js
+++ brag/3.tests-pyramid/1.home-page.spec.js
@@ -8,16 +8,27 @@ describe('home page', () => {
     });
 
     it('offers prime factors decomposition', async () => {
-        page.set('Number to decompose').value = '42';
+        page.enter('Number to decompose', '15');
         page.click('compute');
 
         await eventually(() =>
-            expect(page.section('Results')).to.contain('42 = 2 x 3 x 7')
+            expect(page.section('Results')).to.contain('15 = 3 x 5')
         );
     });
 
     it('gives focus to input field', () => {
         expect(page.document.activeElement).to.equal(page.input('Number to decompose'));
+    });
+
+    it('displays an example', () => {
+        expect(page.input('Number to decompose').value).to.equal('42');
+        expect(page.section('Results')).to.contain('42 = 2 x 3 x 7');
+    });
+
+    it('clears results on input', () => {
+        page.enter('Number to decompose', '15');
+
+        expect(page.section('Results')).to.equal('Results');
     });
 });
 
