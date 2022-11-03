658c4f48bfc988a4d7ac3471436add5998803f1e
ericminio <emignot@change.org>
2022-11-03 11:34:39 -0700

    custom fetch

--- brag/4.api/start.mjs
+++ brag/4.api/start.mjs
@@ -3,8 +3,16 @@ import { contentOfFile, Server } from '../../lib/index.js';
 const indexFile = new URL('./index.html', import.meta.url);
 const html = contentOfFile(indexFile);
 const handler = (incoming, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end(html);
+    if (incoming.url.startsWith('/decompose')) {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({
+            decomposition: [2, 3, 7]
+        }));
+    }
+    else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(html);
+    }
 };
 
 export const server = new Server(5001, handler);
