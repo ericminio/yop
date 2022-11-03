034a977f785976225707a284edef67556856f82a
ericminio <eric.mignot@gmail.com>
2022-11-03 09:02:24 -0700

    about api

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ Outside-in Test-Driven Development
 
 - [Hello world](https://ericminio.github.io/yop/brag/1.hello-world/)
 
-- [Async UI](https://ericminio.github.io/yop/brag/2.async-ui/)
+- [Async UI](https://ericminio.github.io/yop/brag/2.async/)
+
+- [Test audience](https://ericminio.github.io/yop/brag/3.test-audience/)
+
+- [Api](https://ericminio.github.io/yop/brag/4.api/)
 
---
+++ brag/4.api/1.home-page.spec.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { page, eventually } from '../../lib/index.js';
+
+describe('home page', () => {
+
+    beforeEach(async () => {
+        await page.open(new URL('./index.html', import.meta.url));
+    });
+
+    it('offers prime factors decomposition', async () => {
+        page.set('Number to decompose').value = '42';
+        page.click('compute');
+
+        await eventually(() =>
+            expect(page.section('Results')).to.contain('42 = 2 x 3 x 7')
+        );
+    });
+});
+
