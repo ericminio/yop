4d4cfec70e9f1cfc8a6d60d942e69906aeff3aca
ericminio <eric.mignot@gmail.com>
2022-11-03 22:11:51 -0700

    fetch can POST

--- about/fetch.spec.js
+++ about/fetch.spec.js
@@ -1,16 +1,19 @@
 import { expect } from 'chai';
-import { fetch, Server } from '../lib/index.js';
+import { fetch, payload, Server } from '../lib/index.js';
 
 describe('fetch', () => {
 
     let server;
     beforeEach(done => {
         server = new Server(5001, (incoming, response) => {
-            response.writeHead(200, { 'Content-Type': 'application/json' });
-            response.end(JSON.stringify({
-                method: incoming.method,
-                url: incoming.url
-            }));
+            payload(incoming).then(body => {
+                response.writeHead(200, { 'Content-Type': 'application/json' });
+                response.end(JSON.stringify({
+                    method: incoming.method,
+                    url: incoming.url,
+                    body,
+                }));
+            });
         });
         server.start(() => done());
     });
@@ -23,6 +26,14 @@ describe('fetch', () => {
         const response = await request('/hello');
         const data = await response.json();
 
-        expect(data).to.deep.equal({ method: 'GET', url: '/hello' });
+        expect(data).to.deep.equal({ method: 'GET', url: '/hello', body: '' });
+    });
+
+    it('can POST', async () => {
+        const request = fetch(5001);
+        const response = await request('/hello', { method: 'POST', body: 'hello world' });
+        const data = await response.json();
+
+        expect(data).to.deep.equal({ method: 'POST', url: '/hello', body: 'hello world' });
     });
 });
