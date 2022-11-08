bcdcbcf9fcc03b1c4fcaad2effad6bd7a66999bb
ericminio <eric.mignot@gmail.com>
2022-11-08 07:23:23 -0800

    leaner

--- brag/4.api/start.mjs
+++ brag/4.api/start.mjs
@@ -3,8 +3,7 @@ import { serveDecomposition } from './serve-decomposition.js';
 
 const router = new Router([
     { matches: (incoming) => incoming.url.startsWith('/decompose'), go: serveDecomposition },
-    { matches: (incoming) => incoming.url !== '/', go: serveAssets(new URL('.', import.meta.url)) },
-    { matches: () => true, go: serveAsset(new URL('./index.html', import.meta.url)) }
+    { matches: () => true, go: serveAssets(new URL('.', import.meta.url)) },
 ]);
 
 export const server = new Server(5001, router.handler.bind(router));
