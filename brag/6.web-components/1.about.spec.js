789b48025c6c72ed7a7d0bcafbf4b99fdfd51933
ericminio <emignot@change.org>
2023-05-19 14:55:47 -0700

    About web components

---
+++ brag/6.web-components/1.about.spec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { page } from '../../lib/index.js';
+
+describe('Web component', () => {
+    beforeEach(async () => {
+        await page.open(new URL('./index.html', import.meta.url));
+    });
+
+    it('is a way to reuse code', () => {
+        expect(page.section('Todos')).to.contain('Todo: Enjoy');
+    });
+});
