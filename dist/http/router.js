eb16d92255226107214eba00e7fa18335f247f63
ericminio <eric.mignot@gmail.com>
2023-07-12 22:58:45 -0700

    router

--- dist/http/router.js
+++ dist/http/router.js
@@ -2,8 +2,22 @@ export class Router {
     constructor(routes) {
         this.routes = routes;
     }
-    handler(incoming, response) {
-        const route = this.routes.find((r) => r.matches(incoming));
-        route.go(incoming, response);
+    async handler(incoming, response) {
+        let found = false;
+        for (let i = 0; i < this.routes.length; i++) {
+            let route = this.routes[i];
+            let matching = await route.matches(incoming, this);
+            if (matching) {
+                found = true;
+                await route.go(incoming, response, this);
+                break;
+            }
+        }
+        if (!found) {
+            response.statusCode = 404;
+            response.setHeader('content-type', 'text/plain');
+            response.write('NOT FOUND');
+            response.end();
+        }
     }
 }
