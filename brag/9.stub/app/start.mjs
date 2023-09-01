7cae0d39cfb5538fa45bf3f9644784c03c903409
ericminio <eric.mignot@gmail.com>
2023-09-01 14:52:45 -0700

    separate routes

--- brag/9.stub/app/start.mjs
+++ brag/9.stub/app/start.mjs
@@ -7,23 +7,25 @@ import {
 
 const router = new Router([
     {
+        matches: () => fileExists(process.env.YOP_STUB_FILE),
+        go: (_, response) => {
+            const answer = contentOfFile(process.env.YOP_STUB_FILE);
+            response.writeHead(200, {
+                'content-type': 'application/json',
+                'content-length': answer.length,
+            });
+            response.end(answer);
+        },
+    },
+    {
         matches: () => true,
         go: (_, response) => {
-            if (fileExists(process.env.YOP_STUB_FILE)) {
-                const answer = contentOfFile(process.env.YOP_STUB_FILE);
-                response.writeHead(200, {
-                    'content-type': 'application/json',
-                    'content-length': answer.length,
-                });
-                response.end(answer);
-            } else {
-                const answer = 'NOT FOUND';
-                response.writeHead(404, {
-                    'content-type': 'text/plain',
-                    'content-length': answer.length,
-                });
-                response.end(answer);
-            }
+            const answer = 'NOT FOUND';
+            response.writeHead(404, {
+                'content-type': 'text/plain',
+                'content-length': answer.length,
+            });
+            response.end(answer);
         },
     },
 ]);
