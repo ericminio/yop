dd8529f14f479d89095f4ca96ca83f2910907be4
ericminio <eric.mignot@gmail.com>
2023-11-07 22:28:06 -0800

    separate test files

---
+++ about/testing/page/1.page-opening-file.test.js
@@ -0,0 +1,22 @@
+import { describe, it, before, after } from 'node:test';
+import { strict as assert } from 'node:assert';
+
+import { URL } from 'url';
+import { Page } from '../../../dist/index.js';
+
+describe('page', () => {
+    const file = new URL('./page-index.html', import.meta.url);
+    let page;
+
+    before(async () => {
+        page = new Page();
+        await page.open(file);
+    });
+    after(async () => {
+        await page.close();
+    });
+
+    it('works', async () => {
+        assert.equal(await page.title(), 'well done page!');
+    });
+});
---
+++ about/testing/page/2.page-opening-url.test.js
@@ -0,0 +1,29 @@
+import { describe, it, before, after } from 'node:test';
+import { strict as assert } from 'node:assert';
+
+import { Server, html, serveContent } from '../../../dist/index.js';
+import { URL } from 'url';
+import { Page } from '../../../dist/index.js';
+
+describe('page', () => {
+    const file = new URL('./page-index.html', import.meta.url);
+    let server;
+    let baseUrl;
+    let page;
+
+    before(async () => {
+        page = new Page();
+        server = new Server(serveContent(html(file)));
+        const port = await server.start();
+        baseUrl = `http://localhost:${port}`;
+        await page.open(`${baseUrl}`);
+    });
+    after(async () => {
+        await server.stop();
+        await page.close();
+    });
+
+    it('works', async () => {
+        assert.equal(await page.title(), 'well done page!');
+    });
+});
