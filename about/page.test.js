16c53f49c982f30c27cfa3a2675dd25ac0c3adfb
ericminio <eric.mignot@gmail.com>
2023-11-07 22:12:20 -0800

    page can open url

--- about/page.test.js
+++ about/page.test.js
@@ -1,22 +1,43 @@
-import { describe, it, beforeEach, afterEach } from 'node:test';
+import { describe, it, before, after, beforeEach, afterEach } from 'node:test';
 import { strict as assert } from 'node:assert';
 
-import { serveAsset, eventually, Server } from '../dist/index.js';
+import { Server, html, serveContent } from '../dist/index.js';
 import { Page } from '../dist/testing/page.js';
 import { URL } from 'url';
 
-describe('page jsdom', () => {
+describe('page', () => {
+    const file = new URL('./page-index.html', import.meta.url);
     let page;
 
-    beforeEach(() => {
+    before(() => {
         page = new Page();
     });
 
     describe('opening file', () => {
-        beforeEach(async () => {
-            await page.open(new URL('./page-index.html', import.meta.url));
+        before(async () => {
+            await page.open(file);
         });
-        afterEach(async () => {
+        after(async () => {
+            await page.close();
+        });
+
+        it('works', async () => {
+            assert.equal(await page.title(), 'well done page!');
+        });
+    });
+
+    describe('opening url', () => {
+        let server;
+        let baseUrl;
+
+        before(async () => {
+            server = new Server(serveContent(html(file)));
+            const port = await server.start();
+            baseUrl = `http://localhost:${port}`;
+            await page.open(`${baseUrl}`);
+        });
+        after(async () => {
+            await server.stop();
             await page.close();
         });
 
