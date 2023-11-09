9d2bf34a7acae5e162d5d2e8e05c3d959fafa22a
Eric Mignot <eric.mignot@gmail.com>
2023-11-09 11:25:58 -0800

    can click with webdriver too

--- dist/testing/page-webdriver.js
+++ dist/testing/page-webdriver.js
@@ -42,4 +42,24 @@ export class Page {
             throw new Error(`Unable to locate section`);
         }
     }
+
+    async click(text) {
+        try {
+            const buttons = await this.driver.findElements(By.css('button'));
+            const candidates = [];
+            for (let i = 0; i < buttons.length; i++) {
+                const candidate = buttons[i];
+                const actual = await candidate.getText();
+                if (actual.indexOf(text) !== -1) {
+                    candidates.push({ element: candidate, text: actual });
+                }
+            }
+            const selected = candidates.sort(
+                (a, b) => a.text.length - b.text.length
+            )[0];
+            await selected.element.click();
+        } catch (error) {
+            throw new Error(`Unable to locate button`);
+        }
+    }
 }
