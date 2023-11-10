c1052ebdbec735b0a3689cd359529b526296fcf1
Eric Mignot <eric.mignot@gmail.com>
2023-11-10 14:52:00 -0800

    about countdown

--- brag/8.timer/about/countdown.test.js
+++ brag/8.timer/about/countdown.test.js
@@ -9,6 +9,12 @@ describe('timers - Count down', () => {
         page = new Page();
         const port = await server.start();
         await page.open(`http://localhost:${port}`);
+        await eventually(page, async () => {
+            assert.match(
+                await page.section('Welcome'),
+                /Start whenever you are ready/
+            );
+        });
     });
     afterEach(async () => {
         await page.close();
@@ -16,36 +22,24 @@ describe('timers - Count down', () => {
     });
 
     it('can be triggered', async () => {
-        await eventually(page, () => {
-            assert.match(
-                page.section('Welcome'),
-                /Start whenever you are ready/
-            );
-        });
-        page.click('Start');
+        await page.click('Start');
 
-        await eventually(page, () => {
-            assert.match(page.section('Timer'), /Remaining 14s/);
-            assert.doesNotMatch(page.section('Timer'), /Done/);
+        await eventually(page, async () => {
+            assert.match(await page.section('Timer'), /Remaining 14s/);
+            assert.doesNotMatch(await page.section('Timer'), /Done/);
         });
     });
 
     it('notifies when done', async () => {
-        await eventually(page, () => {
-            assert.match(
-                page.section('Welcome'),
-                /Start whenever you are ready/
-            );
-        });
         page.element('home-page').yoptimer = new page.window.YopTimer({
             delay: 50,
             count: 3,
         });
-        page.click('Start');
+        await page.click('Start');
 
-        await eventually(page, () => {
-            assert.match(page.section('Timer'), /Remaining 0s/);
-            assert.match(page.section('Timer'), /Done/);
+        await eventually(page, async () => {
+            assert.match(await page.section('Timer'), /Remaining 0s/);
+            assert.match(await page.section('Timer'), /Done/);
         });
     });
 });
