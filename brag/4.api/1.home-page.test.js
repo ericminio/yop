ab8d3f194219a4bf693087af7cd5f431590f52fb
Eric Mignot <eric.mignot@gmail.com>
2023-11-10 13:18:09 -0800

    api brag

--- brag/4.api/1.home-page.test.js
+++ brag/4.api/1.home-page.test.js
@@ -1,26 +1,26 @@
-import { describe, it, beforeEach, afterEach } from 'node:test';
+import { describe, it, before, after } from 'node:test';
 import { strict as assert } from 'node:assert';
 import { Page, eventually } from '../../dist/index.js';
 import { server } from './start.mjs';
 
 describe('api - home page', () => {
     let page;
-    beforeEach(async () => {
+    before(async () => {
         page = new Page();
         const port = await server.start();
         await page.open(`http://localhost:${port}`);
     });
-    afterEach(async () => {
+    after(async () => {
         await page.close();
         await server.stop();
     });
 
     it('offers prime factors decomposition', async () => {
-        page.enter('Number to decompose', '42');
-        page.click('compute');
+        await page.enter('Number to decompose', '42');
+        await page.click('compute');
 
-        await eventually(() =>
-            assert.match(page.section('Results'), /42 = 2 x 3 x 7/)
+        await eventually(async () =>
+            assert.match(await page.section('Results'), /42 = 2 x 3 x 7/)
         );
     });
 });
