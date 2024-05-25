19a54b70e5c7003ecfc428abed08f0499b3088e3
ericminio <eric.mignot@gmail.com>
2024-05-25 13:04:45 -0700

    dead code removed

--- brag/10.hexagonal/app/web/about/servers.js
+++
@@ -1,7 +0,0 @@
-import { Server } from '../../../../../dist/index.js';
-import { routerForComponent } from './routers.js';
-
-export const serverForComponent = (componentClass, body) => {
-    const router = routerForComponent(componentClass, body);
-    return new Server(router.handler.bind(router));
-};
