975d1384bd31ad5f94e632b6413ee15c4ea2cdd3
Eric Mignot <eric.mignot@gmail.com>
2024-08-27 14:40:30 -0700

    pretend that auto reload happens

--- about/reload/about/reload.test.js
+++ about/reload/about/reload.test.js
@@ -7,13 +7,16 @@ import {
     afterEach,
 } from 'node:test';
 import { strict as assert } from 'node:assert';
-import { eventually, Page } from '../../../dist/index.js';
+import { contentOfFile, eventually, Page } from '../../../dist/index.js';
 import { server } from '../app/web/server.js';
+import { writeFileSync } from 'node:fs';
 
-describe('hot reload', () => {
+describe('YOP_AUTO_RELOAD', () => {
     let page;
     let baseUrl;
     before(async () => {
+        process.env.YOP_AUTO_RELOAD = 1;
+
         const port = await server.start();
         baseUrl = `http://localhost:${port}`;
     });
@@ -22,15 +25,25 @@ describe('hot reload', () => {
     });
     beforeEach(async () => {
         page = new Page();
-        await page.open(baseUrl);
     });
     afterEach(async () => {
         await page.close();
     });
 
-    test('is available on request', async () => {
+    test('enables automatic reload when source changes', async () => {
+        const content = contentOfFile(
+            new URL('./content.html', import.meta.url)
+        );
+        const file = new URL('../app/web/ignore.html', import.meta.url);
+        writeFileSync(file, content);
+        await page.open(baseUrl);
         await eventually(page, async () => {
             assert.match(await page.section('Reload'), /Hello world/);
+        });
+        writeFileSync(file, content.replace('Hello world', 'Hello universe'));
+
+        await eventually(page, async () => {
+            assert.match(await page.section('Reload'), /Hello universe/);
         });
     });
 });
--- about/reload/app/web/server.js
+++ about/reload/app/web/server.js
@@ -1,7 +1,7 @@
 import { RouteDefault, Router, Server, html } from '../../../../dist/index.js';
 
 const router = new Router([
-    new RouteDefault(html(new URL('./index.html', import.meta.url))),
+    new RouteDefault(html(new URL('./ignore.html', import.meta.url))),
 ]);
 
 export const server = new Server(router.handler.bind(router));
