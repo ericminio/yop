8631eb47e07c72209295b3d1bdadc194f2ea92e4
ericminio <eric.mignot@gmail.com>
2024-08-27 21:19:52 -0700

    docker compose

--- .github/workflows/support/wait.sh
+++ .github/workflows/support/wait.sh
@@ -7,7 +7,7 @@ ready=0
 while [ $ready != 1 ]
 do
     echo "waiting for database";
-    docker-compose exec -T db psql yop -U dev -q -c "select 'yes' DATABASE_IS_READY" > $DIR/wait.log
+    docker compose exec -T db psql yop -U dev -q -c "select 'yes' DATABASE_IS_READY" > $DIR/wait.log
     ready=`grep yes $DIR/wait.log | wc -l`
     sleep 1;
 done;
